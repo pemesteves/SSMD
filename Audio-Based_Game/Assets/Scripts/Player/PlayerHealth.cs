a9bc77077f77cd59c7628d51d8b1fa8374cd4e46
Pedro Esteves <up201705160@fe.up.pt>
2022-01-25 21:45:32 +0000

    Player health values changing

--- Audio-Based_Game/Assets/Scripts/Player/PlayerHealth.cs
+++ Audio-Based_Game/Assets/Scripts/Player/PlayerHealth.cs
@@ -7,6 +7,10 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private LibPdInstance heartBeatInstance = null;
     [SerializeField] private float maxHealth = 100f;
 
+    [SerializeField] private Vector3 damageRadius = Vector3.one;
+    [SerializeField] private float enemyDamagePerSecond = 1f, healthRecoverPerSecond = .5f;
+    [SerializeField] private LayerMask enemyLayerMask;
+
     [SerializeField] private GameObject scream = null;
 
     private float currentHealth;
@@ -19,27 +23,49 @@ public class PlayerHealth : MonoBehaviour
         heartBeatInstance.SendBang(HeartBeatBang);
     }
 
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawCube(transform.position, damageRadius);
+    }
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Damage(5);
-        }
+        if (currentHealth <= 0) return;
+
+        Collider[] _enemies = Physics.OverlapBox(transform.position, damageRadius, Quaternion.identity, enemyLayerMask);
+
+        if (_enemies.Length > 0) Damage(_enemies.Length * enemyDamagePerSecond * Time.deltaTime);
+        else Recover();
+
+        Debug.Log(currentHealth);
     }
 
     private void Damage(float damage)
     {
         if (currentHealth <= 0) return;
 
-        currentHealth = Mathf.Clamp(currentHealth - damage, 0, maxHealth);
-
-        UpdateHeartBeat();
+        SetCurrentHealth(currentHealth - damage);
 
         if (currentHealth > 0) return;
+
         heartBeatInstance.SendBang(HeartBeatBang);
         Instantiate(scream, transform);
         Player.instance.Movement.enabled = false;
         Player.instance.Shoot.enabled = false;
+    }
+
+    private void Recover()
+    {
+        if (currentHealth <= 0) return;
+
+        SetCurrentHealth(currentHealth + healthRecoverPerSecond * Time.deltaTime);
+    }
+
+    private void SetCurrentHealth(float healthValue)
+    {
+        currentHealth = Mathf.Clamp(healthValue, 0, maxHealth);
+        UpdateHeartBeat();
     }
 
     private void UpdateHeartBeat() => heartBeatInstance.SendFloat(HealthValue, currentHealth / maxHealth);
