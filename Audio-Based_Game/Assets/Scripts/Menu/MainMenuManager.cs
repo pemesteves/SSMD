bd6ba8c7740bbf61aed84b2f263799584920eff5
Pedro Esteves <up201705160@fe.up.pt>
2022-01-26 12:57:13 +0000

    Added options menu

--- Audio-Based_Game/Assets/Scripts/Menu/MainMenuManager.cs
+++ Audio-Based_Game/Assets/Scripts/Menu/MainMenuManager.cs
@@ -3,17 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    [SerializeField] private GameObject mainPanel = null, instructionsPanel = null;
+    [SerializeField] private GameObject mainPanel = null, instructionsPanel = null, optionsPanel = null;
 
     private void Start() => MainMenu();
     public void Play() => SceneManager.LoadScene(SceneNames.Game);
-    public void MainMenu() => EnableMenu(true, false);
-    public void Instructions() => EnableMenu(false, true);
+    public void MainMenu() => EnableMenu(true, false, false);
+    public void Instructions() => EnableMenu(false, true, false);
+    public void Options() => EnableMenu(false, false, true);
     public void Exit() => Application.Quit();
 
-    private void EnableMenu(bool main, bool instructions)
+    private void EnableMenu(bool main, bool instructions, bool options)
     {
         mainPanel.SetActive(main);
         instructionsPanel.SetActive(instructions);
+        optionsPanel.SetActive(options);
     }
+
+    public void OnAccessibilityToggleChanged(bool newValue) => UAP_AccessibilityManager.EnableAccessibility(newValue, true);
 }
