8c4b3a3af4d485cd3917ca6c9d0ad9e0dfe46b4e
Pedro Esteves <up201705160@fe.up.pt>
2022-01-26 22:17:26 +0000

    Saving options on player prefs

--- Audio-Based_Game/Assets/Scripts/Menu/MainMenuManager.cs
+++ Audio-Based_Game/Assets/Scripts/Menu/MainMenuManager.cs
@@ -1,11 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject mainPanel = null, instructionsPanel = null, optionsPanel = null;
 
-    private void Start() => MainMenu();
+    [SerializeField] private Toggle optionsToggle = null;
+    private const string AccessibilityKey = "accessibility";
+
+    private void Start()
+    {
+        MainMenu();
+
+        bool isOn = PlayerPrefs.GetInt(AccessibilityKey, 1) == 1;
+        optionsToggle.isOn = isOn;
+        OnAccessibilityToggleChanged(isOn);
+    }
+
     public void Play() => SceneManager.LoadScene(SceneNames.Game);
     public void MainMenu() => EnableMenu(true, false, false);
     public void Instructions() => EnableMenu(false, true, false);
@@ -19,5 +31,10 @@ public class MainMenuManager : MonoBehaviour
         optionsPanel.SetActive(options);
     }
 
-    public void OnAccessibilityToggleChanged(bool newValue) => UAP_AccessibilityManager.EnableAccessibility(newValue, true);
+    public void OnAccessibilityToggleChanged(bool newValue)
+    {
+        UAP_AccessibilityManager.EnableAccessibility(newValue, true);
+        PlayerPrefs.SetInt(AccessibilityKey, newValue ? 1 : 0);
+        PlayerPrefs.Save();
+    }
 }
